3247427c5dfdae9ac8e5c963a3d0640bda338efa
HuJix <1668819658@qq.com>
2024-03-07 18:13:50 +0800

    🎨 修改分析文件的判断条件

--- config/plugin/visualizer.ts
+++ config/plugin/visualizer.ts
@@ -2,10 +2,11 @@
  * Generation packaging analysis
  * 生成打包分析
  */
-import visualizer from "rollup-plugin-visualizer";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default function configVisualizerPlugin() {
-  if (process.env.REPORT === "true") {
+  console.info("📦 打包分析已开启...");
+  if (process.env.NODE_ENV === "production") {
     return visualizer({
       filename: "./node_modules/.cache/visualizer/stats.html",
       open: true,
