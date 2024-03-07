7bf2adb1657bc45b41fe2ee361ed5f22eb5420ac
HuJix <1668819658@qq.com>
2024-03-07 16:45:41 +0800

    🌐 修改页面展示，添加国际化的示例

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
+import { LOCALE_OPTIONS, LocaleOption } from "@/locale";
+import LanguageSVG from "assets/icons/language.svg?component";
+import VueSVG from "assets/icons/vue.svg?component";
 import { ref } from "vue";
 
 const isOpen = ref(false);
+
+const changeLocale = (item: LocaleOption) => {
+  localStorage.setItem("lang", item.value);
+};
 </script>
 
 <template>
@@ -15,97 +22,46 @@ const isOpen = ref(false);
           viewBox="0 0 24 24"
           stroke-width="1.5"
           stroke="currentColor"
-          class="h-6 w-6"
+          class="h-6 w-6 text-white"
         >
           <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
         </svg>
-        <img class="h-6 w-auto sm:h-7" src="/src/assets/vue.svg" alt="vue" />
+        <VueSVG class="h-6 w-auto sm:h-7" />
       </a>
-
-      <!-- Mobile menu button -->
-      <div class="flex lg:hidden">
-        <button
-          @click="isOpen = !isOpen"
-          type="button"
-          class="text-gray-500 hover:text-gray-600 focus:text-gray-600 focus:outline-none dark:text-gray-200 dark:hover:text-gray-400 dark:focus:text-gray-400"
-          aria-label="toggle menu"
-        >
-          <svg
-            v-show="!isOpen"
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-          >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16" />
-          </svg>
-
-          <svg
-            v-show="isOpen"
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-          >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
-      </div>
     </div>
 
-    <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
     <div
-      :class="[isOpen ? 'translate-x-0 opacity-100 ' : '-translate-x-full opacity-0']"
-      class="absolute inset-x-0 z-20 w-full bg-white px-6 py-4 shadow-md transition-all duration-300 ease-in-out lg:relative lg:top-0 lg:mt-0 lg:flex lg:w-auto lg:translate-x-0 lg:items-center lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none dark:bg-gray-900 lg:dark:bg-transparent"
+      class="absolute inset-x-0 z-20 w-full gap-2 px-6 shadow-md transition-all duration-300 ease-in-out lg:relative lg:top-0 lg:mt-0 lg:flex lg:w-auto lg:translate-x-0 lg:items-center lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none dark:bg-gray-900 lg:dark:bg-transparent"
     >
-      <div class="lg:-px-8 flex flex-col space-y-4 lg:mt-0 lg:flex-row lg:space-y-0">
-        <a
-          class="transform text-gray-700 transition-colors duration-300 hover:text-blue-500 lg:mx-8 dark:text-gray-200 dark:hover:text-blue-400"
-          href="#"
+      <div class="relative inline-block">
+        <button
+          @click="isOpen = !isOpen"
+          class="relative z-10 block rounded-md border-none border-transparent p-2 focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:focus:ring-blue-400 dark:focus:ring-opacity-40"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6">
-            <path
-              d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z"
-            />
-            <path
-              d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z"
-            />
-          </svg>
-        </a>
-        <a
-          class="transform text-gray-700 transition-colors duration-300 hover:text-blue-500 lg:mx-8 dark:text-gray-200 dark:hover:text-blue-400"
-          href="https://github.com/hujix/vite-vue-starter"
+          <LanguageSVG class="text-white" />
+        </button>
+        <div
+          v-show="isOpen"
+          @click="isOpen = false"
+          class="absolute right-0 z-20 mt-2 w-auto origin-top-right rounded-md py-2 shadow-xl ease-out dark:bg-gray-800"
         >
-          <svg
-            class="h-5 w-5"
-            viewBox="0 0 20 20"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
+          <a
+            href="#"
+            v-for="(item, index) in LOCALE_OPTIONS"
+            :key="index"
+            @click="changeLocale(item)"
+            class="block transform px-4 py-2 text-sm capitalize text-gray-600 transition-colors duration-300 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white"
           >
-            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-              <g id="Dribbble-Light-Preview" transform="translate(-140.000000, -7559.000000)" fill="#000000">
-                <g id="icons" transform="translate(56.000000, 160.000000)">
-                  <path
-                    d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399"
-                    id="github-[#142]"
-                  ></path>
-                </g>
-              </g>
-            </g>
-          </svg>
-        </a>
+            {{ item.label }}
+          </a>
+        </div>
       </div>
 
       <a
         class="mt-4 block rounded-lg bg-blue-600 px-5 py-2 text-center text-sm capitalize text-white hover:bg-blue-500 lg:mt-0 lg:w-auto"
         href="#"
       >
-        Get started
+        {{ $t("header.button") }}
       </a>
     </div>
   </nav>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useUserStore } from "@/store/modules/user";
+import VueSVG from "assets/icons/vue.svg?component";
 import { onMounted } from "vue";
 
 const userStore = useUserStore();
@@ -18,19 +19,23 @@ onMounted(() => {
         viewBox="0 0 24 24"
         stroke-width="1.5"
         stroke="currentColor"
-        class="h-6 w-6"
+        class="h-6 w-6 text-white"
       >
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
       </svg>
-      <img class="h-24 w-auto sm:h-20" src="/src/assets/vue.svg" alt="vue" />
+      <VueSVG class="h-24 w-auto sm:h-20" />
     </a>
     <div class="mx-auto mt-24 max-w-lg">
       <h1 class="text-3xl font-semibold text-gray-800 lg:text-4xl dark:text-white">
-        Building Your Web App with this Starter Template
+        {{ $t("home.title") }}
       </h1>
+      <p
+        class="underline-offset-3 text-2xl font-semibold text-gray-800 underline decoration-blue-400 decoration-8 lg:text-4xl dark:text-white dark:decoration-blue-600"
+      >
+        By {{ userStore.name }}
+      </p>
       <p class="mt-6 text-gray-500 dark:text-gray-300">
-        Start developing web applications immediately with no or minimal configuration required
-        <!-- 无需或只需配置，立即开始开发网络应用程序 -->
+        {{ $t("home.desc") }}
       </p>
       <a
         type="button"
@@ -38,8 +43,7 @@ onMounted(() => {
         href="https://github.com/hujix/vite-vue-starter"
         class="mt-6 rounded-lg bg-blue-600 px-5 py-2 text-center text-sm font-medium capitalize leading-5 text-white hover:bg-blue-500 focus:outline-none lg:mx-0 lg:w-auto"
       >
-        Let's get started with
-        {{ userStore.name }}
+        {{ $t("home.button") }}
       </a>
     </div>
   </div>
